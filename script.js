0306b21aa3cf9b9bcbb3c8c018dd7c322716d3cd
alexislindeman <angelalexis09@msn.com>
2017-02-09 20:03:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+/*
+// STEP 1 
+var someMonth;
+function theMonth 		// function to return current month
+currentMonth 		// a constant
+var summerMonth; 		// an array of summer months
+myLibraryAfunction // a function 
+
+
+// STEP 2
+21      //numerical
+"Happy Birthday"        //string literal
+true        //boolean literal
+null        //literal null
+
+
+// STEP 3
+var anExpression = 10 * (8 - 5) / 6;
+var anotherExpression = anExpression + "%" + anExpression;
+
+
+//STEP 4
+var firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou;
+
+
+//STEP 5
+var firstName;
+var lastName;
+firstName = "Alexis";
+lastName = "Lindeman";
+
+var address = "9924 Kika Ct";
+var city = "San Diego";
+var state ="CA";
+var zipCode ="92129";
+
+var yourAge = "24", referralSource = "friend", mayWeContactYou = "yes";
+
+
+//STEP 6
+var yearsAlive = 24 + "two";
+console.log(yearsAlive);
+
+var likesDogs = true + "Happy";
+console.log(likesDogs);
+
+var howMany = 12 + false;
+console.log(howMany);
+
+
+//STEP 7
+var food;
+console.log(food);
+food = "macaroni";
+
+
+//STEP 8 This string is not literal valid. It requires backslashes to display the quotes.
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+console.log(someString);
+
+
+//STEP 9
+var x = null;
+console.log(x);
+
+var lemons;
+console.log(lemons);
+
+
+//STEP 10
+console.log(typeof "Superman");
+console.log(typeof 1);
+console.log(typeof true);
+console.log(typeof {brand: 'Gucci', size: 2});
+console.log(typeof shirtColor);
+
+
+//STEP 11
+alert("Hello " + "Alexis Lindeman" + ", welcome to the JavaScript class!");
+
+
+//STEP 12
+var name = "Alexis Lindeman";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+
+
+//STEP 13
+var name = "Alexis Lindeman";
+var course = "JavaScript";
+alert("Hello " + name + ", welcome to the "+ course +" class!");
+
+
+//STEP 14
+var name = "Alexis Lindeman";
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the "+ course +" class!");
+
+
+//STEP 15
+var name = prompt("What is your name?");
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the "+ course +" class!");
+
+
+//STEP 16
+var name = prompt("What is your name?");
+var course = prompt("What course are you taking?");
+alert("Hello " + name + ".\nWelcome to the "+ course +" class!");
+
+
+//STEP 17
+var x = 10;
+var y = 20;
+console.log(y + x);
+
+
+//STEP 18
+var x = 20;
+console.log(x + 20);
+
+
+//STEP 19
+var x = 20;
+console.log(x * 5);
+
+
+//STEP 20
+var x = 20 % 3;
+console.log(x / 1);
+
+
+//STEP 21
+var x = 5;
+        var y = 10;
+        alert(x == 5 || y == 5);
+
+
+//STEP 22       
+        var x = 5;
+        var y = 10;
+        alert(x <= y != true);
+
+
+//STEP 23
+var widget = new Object();
+console.log(typeof widget);
+
+
+//STEP 24
+var widget = new Object();
+console.log(widget instanceof Object);
+
+
+//STEP 25
+var widget = new Object();
+console.log(widget instanceof String);
+*/
